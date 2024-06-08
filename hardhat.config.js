35014501b5bf73bf5f63b25868848dc43b60c1c1
Sven Reher <dev@sreher.de>
2024-06-08 09:38:56 +0200

    remove networks from the configuration file

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,16 +6,6 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.17",
   defaulthost:"localhost",
-  networks: {
-    sepolia: {
-      url: process.env.ALCHEMY_TESTNET_RPC_URL,
-      accounts: [process.env.ALCHEMY_TESTNET_PRIVATE_KEY],
-    },
-    localhost: {
-      url: process.env.TESTNET_RPC_URL_LOCAL,
-      accounts: [process.env.TESTNET_PRIVATE_KEY_LOCAL]
-    }
-  },
   settings: {
     optimizer: {
       enabled: true,
